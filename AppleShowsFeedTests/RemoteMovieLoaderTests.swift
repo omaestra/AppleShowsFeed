b83d5e29e1d448be753bb0e011fef9b21220d70c
omaestra <omaestra@gmail.com>
2025-09-23 14:47:12 +0200

    Load command delivers connectivity error on http client error.

--- AppleShowsFeedTests/RemoteMovieLoaderTests.swift
+++ AppleShowsFeedTests/RemoteMovieLoaderTests.swift
@@ -9,18 +9,24 @@ import XCTest
 import AppleShowsFeed
 
 protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL) throws
 }
 
 class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    var error: Error?
     
-    func get(from url: URL) {
+    func get(from url: URL) throws {
+        if let error { throw error }
         self.requestedURL = url
     }
 }
 
 class RemoteMovieLoader {
+    public enum Error: Swift.Error {
+        case connectivity
+    }
+    
     let url: URL
     let client: HTTPClient
     
@@ -30,7 +36,11 @@ class RemoteMovieLoader {
     }
     
     func load() async throws -> [Movie] {
-        client.get(from: url)
+        do {
+            try client.get(from: url)
+        } catch {
+            throw Error.connectivity
+        }
         return []
     }
 }
@@ -44,5 +54,19 @@ final class RemoteMovieLoaderTests: XCTestCase {
         _ = try? await sut.load()
         
         XCTAssertNotNil(client.requestedURL)
+    }
+    
+    func test_load_deliversErrorOnClientError() async {
+        let url = URL(string: "http://any-url.com")!
+        let client = HTTPClientSpy()
+        client.error = NSError(domain: "any error", code: -1)
+        let sut = RemoteMovieLoader(url: url, client: client)
+        
+        do {
+            _ = try await sut.load()
+            XCTFail("Expected failure, got success instead")
+        } catch {
+            XCTAssertEqual(error as? RemoteMovieLoader.Error, .connectivity)
+        }
     }
 }
