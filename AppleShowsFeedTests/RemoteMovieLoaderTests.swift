71ce66ef1c665267d0bc0b3a7b1e21ff7949eeaf
omaestra <omaestra@gmail.com>
2025-09-23 14:37:33 +0200

    Load command requests data from URL.

--- AppleShowsFeedTests/RemoteMovieLoaderTests.swift
+++ AppleShowsFeedTests/RemoteMovieLoaderTests.swift
@@ -6,7 +6,36 @@
 //
 
 import XCTest
+import AppleShowsFeed
+
+class HTTPClient {
+    var requestedURL: URL?
+    
+    func get(from url: URL) {
+        self.requestedURL = url
+    }
+}
+
+class RemoteMovieLoader {
+    var client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() async throws -> [Movie] {
+        client.get(from: URL(string: "https://some-url.com")!)
+        return []
+    }
+}
 
 final class RemoteMovieLoaderTests: XCTestCase {
-
+    func test_load_requestsDataFromURL() async {
+        let client = HTTPClient()
+        let sut = RemoteMovieLoader(client: client)
+        
+        _ = try? await sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+    }
 }
