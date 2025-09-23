58bd034cb0210f3b21729f10260ab6f81e9cddcb
omaestra <omaestra@gmail.com>
2025-09-23 14:23:47 +0200

    Create RemoteMovieLoaderTests file.

---
+++ AppleShowsFeedTests/RemoteMovieLoaderTests.swift
@@ -0,0 +1,12 @@
+//
+//  RemoteMovieLoaderTests.swift
+//  AppleShowsFeedTests
+//
+//  Created by Oswaldo Maestra on 23/09/2025.
+//
+
+import XCTest
+
+final class RemoteMovieLoaderTests: XCTestCase {
+
+}
