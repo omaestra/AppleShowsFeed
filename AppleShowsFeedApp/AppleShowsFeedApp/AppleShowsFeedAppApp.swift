f028f98f96aef8539be72ca0dca081b5d08204d5
omaestra <omaestra@gmail.com>
2025-09-24 15:43:09 +0200

    Add new iOS project to workspace.

---
+++ AppleShowsFeedApp/AppleShowsFeedApp/AppleShowsFeedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleShowsFeedAppApp.swift
+//  AppleShowsFeedApp
+//
+//  Created by Oswaldo Maestra on 24/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleShowsFeedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleShowsFeedApp/AppleShowsFeedApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleShowsFeedApp
+//
+//  Created by Oswaldo Maestra on 24/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
