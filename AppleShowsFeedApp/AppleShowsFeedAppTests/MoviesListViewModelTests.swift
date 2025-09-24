882587f294120e4cf65cdca5297ea00bb9d395ed
omaestra <omaestra@gmail.com>
2025-09-24 15:49:35 +0200

    Create MoviesListViewModelTests file.

---
+++ AppleShowsFeedApp/AppleShowsFeedAppTests/MoviesListViewModelTests.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesListViewModelTests.swift
+//  AppleShowsFeedAppTests
+//
+//  Created by Oswaldo Maestra on 24/09/2025.
+//
+
+import XCTest
+
+final class MoviesListViewModelTests: XCTestCase {
+    
+}
