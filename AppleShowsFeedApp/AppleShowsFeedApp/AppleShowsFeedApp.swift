4a3644c692dbca967941825ba1fd1a38a6186045
omaestra <omaestra@gmail.com>
2025-09-25 13:41:49 +0200

    Use @ObservedObject property wrapper to manage lifecycle outside from view. Create mock helper function.

--- AppleShowsFeedApp/AppleShowsFeedApp/AppleShowsFeedApp.swift
+++ AppleShowsFeedApp/AppleShowsFeedApp/AppleShowsFeedApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AppleShowsFeedApp: App {
     var body: some Scene {
         WindowGroup {
-            MoviesListView()
+            MoviesListView(viewModel: MoviesListViewModel(movies: .mockData()))
         }
     }
 }
--- AppleShowsFeedApp/AppleShowsFeedApp/MoviesListView.swift
+++ AppleShowsFeedApp/AppleShowsFeedApp/MoviesListView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import AppleShowsFeed
 
 struct MoviesListView: View {
-    @StateObject var viewModel = MoviesListViewModel(movies: [])
+    @ObservedObject var viewModel: MoviesListViewModel
     
     var body: some View {
         List(viewModel.movies, id: \.id) { movie in
@@ -69,12 +69,18 @@ struct MovieCellView: View {
 }
 
 #Preview {
-    MoviesListView(viewModel: MoviesListViewModel(movies: [
-        Movie(id: "1", name: "The Fantastic Four: First Steps", summary: "some summary", title: "The Fantastic Four: First Steps - Matt Shakman", releaseDate: .now, rights: "some rights", rentalPrice: Price(label: "12.99$", amount: 21.99, currency: "USD"), price: Price(label: "21.99$", amount: 21.99, currency: "USD"), artist: "some artist", category: "Action & Adventure", contentType: .movie, duration: 60, images: [
-            ImageItem(url: URL(string: "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/d7/36/81/d736811f-fbe7-285c-c02a-1d746fdb523b/DIS_FANTASTIC_FOUR_FIRST_STEPS_ITUNES_ARTWORK_WW_ARTWORK_EN_2000x3000_508B2600000BMW.lsr/113x170bb.png")!, attributes: .init(height: 20)),
-        ]),
-        Movie(id: "2", name: "Mission: Impossible - The Final Reckoning", summary: "other summary", title: "Mission: Impossible - The Final Reckoning - Christopher McQuarrie", releaseDate: .distantPast, rights: "some rights", rentalPrice: Price(label: "9.99$", amount: 9.99, currency: "USD"), price: Price(label: "12.99$", amount: 12.99, currency: "USD"), artist: "some other artist", category: "Action & Adventure", contentType: .movie, duration: 60, images: [
-            ImageItem(url: URL(string: "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/b2/23/5c/b2235ced-ec7d-7dd5-e993-1d0a594baed2/MI8_CA_2000x3000.jpg/39x60bb.png")!, attributes: .init(height: 20)),
-        ]),
-    ]))
+    MoviesListView(viewModel: MoviesListViewModel(movies: .mockData()))
+}
+
+extension Array where Element == Movie {
+    static func mockData() -> [Movie] {
+        [
+            Movie(id: "1", name: "The Fantastic Four: First Steps", summary: "some summary", title: "The Fantastic Four: First Steps - Matt Shakman", releaseDate: .now, rights: "some rights", rentalPrice: Price(label: "12.99$", amount: 21.99, currency: "USD"), price: Price(label: "21.99$", amount: 21.99, currency: "USD"), artist: "some artist", category: "Action & Adventure", contentType: .movie, duration: 60, images: [
+                ImageItem(url: URL(string: "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/d7/36/81/d736811f-fbe7-285c-c02a-1d746fdb523b/DIS_FANTASTIC_FOUR_FIRST_STEPS_ITUNES_ARTWORK_WW_ARTWORK_EN_2000x3000_508B2600000BMW.lsr/113x170bb.png")!, attributes: .init(height: 20)),
+            ]),
+            Movie(id: "2", name: "Mission: Impossible - The Final Reckoning", summary: "other summary", title: "Mission: Impossible - The Final Reckoning - Christopher McQuarrie", releaseDate: .distantPast, rights: "some rights", rentalPrice: Price(label: "9.99$", amount: 9.99, currency: "USD"), price: Price(label: "12.99$", amount: 12.99, currency: "USD"), artist: "some other artist", category: "Action & Adventure", contentType: .movie, duration: 60, images: [
+                ImageItem(url: URL(string: "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/b2/23/5c/b2235ced-ec7d-7dd5-e993-1d0a594baed2/MI8_CA_2000x3000.jpg/39x60bb.png")!, attributes: .init(height: 20)),
+            ])
+        ]
+    }
 }
