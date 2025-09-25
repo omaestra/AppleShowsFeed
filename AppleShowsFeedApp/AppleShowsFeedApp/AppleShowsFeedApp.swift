be7cf9d72e1301ee1f17469339243a58dd7a9d04
omaestra <omaestra@gmail.com>
2025-09-25 14:55:27 +0200

    Create `MoviesUIListView` class to compose view with dependencies.

--- AppleShowsFeedApp/AppleShowsFeedApp/AppleShowsFeedApp.swift
+++ AppleShowsFeedApp/AppleShowsFeedApp/AppleShowsFeedApp.swift
@@ -6,12 +6,47 @@
 //
 
 import SwiftUI
+import AppleShowsFeed
 
 @main
 struct AppleShowsFeedApp: App {
+    private static let httpClient: HTTPClient = {
+        URLSessionHTTPClient(session: URLSession.shared)
+    }()
+    
+    private static let remoteMovieLoader: RemoteMovieLoader = {
+        let url = URL(string: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/limit=2/json?cc=ca")!
+        return RemoteMovieLoader(url: url, client: httpClient, mapper: MoviesMapper.map)
+    }()
+    
     var body: some Scene {
         WindowGroup {
-            MoviesListView(viewModel: MoviesListViewModel(movies: .mockData()))
+            MoviesUIListView.composedWith(loader: Self.remoteMovieLoader)
         }
+    }
+}
+
+final class MoviesUIListView {
+    static func composedWith(
+        loader: MovieLoader
+    ) -> some View {
+        let viewModel = MoviesListViewModel(movies: [])
+        viewModel.onRefresh = { [loader, weak viewModel] in
+            do {
+                await viewModel?.didStartLoading()
+                let movies = try await loader.load()
+                await viewModel?.didFinishLoading(with: movies)
+            } catch {
+                await viewModel?.didFinishLoading(with: error)
+            }
+        }
+        
+        return MoviesListView(viewModel: viewModel)
+            .task {
+                await viewModel.onRefresh?()
+            }
+            .refreshable {
+                await viewModel.onRefresh?()
+            }
     }
 }
--- AppleShowsFeedApp/AppleShowsFeedApp/ViewModel/MoviesListViewModel.swift
+++ AppleShowsFeedApp/AppleShowsFeedApp/ViewModel/MoviesListViewModel.swift
@@ -8,25 +8,29 @@
 import Foundation
 import AppleShowsFeed
 
-@MainActor
 final class MoviesListViewModel: ObservableObject {
     @Published private(set) var movies: [Movie]
     @Published private(set) var isLoading = false
     @Published private(set) var error: Error?
     
+    public var onRefresh: (() async -> Void)?
+    
     init(movies: [Movie]) {
         self.movies = movies
     }
     
+    @MainActor
     func didStartLoading() {
         isLoading = true
     }
     
+    @MainActor
     func didFinishLoading(with error: Error) {
         self.error = error
         self.isLoading = false
     }
     
+    @MainActor
     func didFinishLoading(with movies: [Movie]) {
         self.movies = movies
         self.isLoading = false
