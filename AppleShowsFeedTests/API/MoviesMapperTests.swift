326008827a4f7dd434c0dd097b1e228d932a3a19
omaestra <omaestra@gmail.com>
2025-09-25 19:34:55 +0200

    Map throws error on non 200 HTTP response.

--- AppleShowsFeedTests/API/MoviesMapperTests.swift
+++ AppleShowsFeedTests/API/MoviesMapperTests.swift
@@ -6,7 +6,22 @@
 //
 
 import XCTest
+import AppleShowsFeed
 
 final class MoviesMapperTests: XCTestCase {
-
+    func test_map_throwsErrorOnNon200HTTPResponse() throws {
+        let url = URL(string: "http://any-url.com")!
+        let itemsJSON = [
+            "feed": ["entry": []]
+        ]
+        let jsonData = try JSONSerialization.data(withJSONObject: itemsJSON)
+        
+        let statusCodeSamples = [199, 201, 300, 400, 500]
+        
+        try statusCodeSamples.forEach { code in
+            XCTAssertThrowsError(
+                try MoviesMapper.map(jsonData, response: HTTPURLResponse(url: url, statusCode: code, httpVersion: nil, headerFields: nil)!)
+            )
+        }
+    }
 }
