b4e07501ca3d0277a14d4c27f1f4edfe2c1f0e3b
omaestra <omaestra@gmail.com>
2025-09-25 18:50:53 +0200

    Create `MovieCellViewModel` class.

--- AppleShowsFeedApp/AppleShowsFeedApp/MoviesListView.swift
+++ AppleShowsFeedApp/AppleShowsFeedApp/MoviesListView.swift
@@ -32,6 +32,31 @@ struct MoviesListView: View {
     }
 }
 
+final class MovieCellViewModel {
+    let id: String
+    let imageURL: URL?
+    let name: String
+    let category: String
+    let rentalPrice: String?
+    let price: String
+    
+    init(
+        id: String,
+        imageURL: URL?,
+        name: String,
+        category: String,
+        rentalPrice: String?,
+        price: String
+    ) {
+        self.id = id
+        self.imageURL = imageURL
+        self.name = name
+        self.category = category
+        self.rentalPrice = rentalPrice
+        self.price = price
+    }
+}
+
 struct MovieCellView: View {
     let movie: Movie
     
