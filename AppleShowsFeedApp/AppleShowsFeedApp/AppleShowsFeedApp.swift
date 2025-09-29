f3a4cf05e6c8a8d913028ac53df7fa7768ac7af9
omaestra <omaestra@gmail.com>
2025-09-29 21:47:38 +0200

    Provide useful comments to give more context about design decisions.

--- AppleShowsFeedApp/AppleShowsFeedApp/AppleShowsFeedApp.swift
+++ AppleShowsFeedApp/AppleShowsFeedApp/AppleShowsFeedApp.swift
@@ -18,6 +18,7 @@ struct AppleShowsFeedApp: App {
     }()
     
     private func makeLoader(for countryCode: String) -> MovieLoader {
+        /// Apple provides JSON RSS feed structure, better for reducing boilerplate and integration with Swift's `Decodable` protocol.
         let url = URL(string: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/limit=100/json?cc=\(countryCode)")!
         return RemoteMovieLoader(url: url, client: Self.httpClient, mapper: MoviesMapper.map)
     }
