a0806d590e6c09331bf8ceaabad4655698c42969
omaestra <omaestra@gmail.com>
2025-09-29 15:25:22 +0200

    Add `SharedTestHelpers.swift` file and shared helper methods to reduce code duplication.

---
+++ AppleShowsFeedTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,22 @@
+//
+//  SharedTestHelpers.swift
+//  AppleShowsFeedTests
+//
+//  Created by Oswaldo Maestra on 29/09/2025.
+//
+
+import Foundation
+
+func anyURL() -> URL {
+    URL(string: "https://any-url.com")!
+}
+
+func anyNSError() -> NSError {
+    NSError(domain: "any error", code: -1)
+}
+
+extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    }
+}
