4b62322a7e221d7046617da23a04b1a4fc2f2911
omaestra <omaestra@gmail.com>
2025-09-30 18:41:03 +0200

    Move `AppComposer` to its own file.

--- AppleShowsFeedApp/AppleShowsFeedApp/AppleShowsFeedApp.swift
+++ AppleShowsFeedApp/AppleShowsFeedApp/AppleShowsFeedApp.swift
@@ -8,23 +8,6 @@
 import SwiftUI
 import AppleShowsFeed
 
-final class AppComposer {
-    private lazy var httpClient: HTTPClient = {
-        URLSessionHTTPClient(session: URLSession.shared)
-    }()
-    
-    func makeMoviesLoader(for countryCode: String) -> MovieLoader {
-        /// Apple provides JSON RSS feed structure, better for reducing boilerplate and integration with Swift's `Decodable` protocol.
-        let url = URL(string: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/limit=100/json?cc=\(countryCode)")!
-        return RemoteMovieLoader(url: url, client: httpClient, mapper: MoviesMapper.map)
-    }
-    
-    convenience init(httpClient: HTTPClient) {
-        self.init()
-        self.httpClient = httpClient
-    }
-}
-
 @main
 struct AppleShowsFeedApp: App {
     /// Hardcoded country value to simply display different feeds by country.
