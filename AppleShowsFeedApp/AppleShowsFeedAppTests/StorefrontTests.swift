145be6637e26b1fe17950a5fd0bd7049ad0a8658
omaestra <omaestra@gmail.com>
2025-09-30 13:28:53 +0200

    Storefront id matches raw value.

--- AppleShowsFeedApp/AppleShowsFeedAppTests/StorefrontTests.swift
+++ AppleShowsFeedApp/AppleShowsFeedAppTests/StorefrontTests.swift
@@ -12,4 +12,9 @@ final class StorefrontTests: XCTestCase {
     func test_allCases_returnsExpectedStorefronts() {
         XCTAssertEqual(Storefront.allCases, [.canada, .spain])
     }
+    
+    func test_id_matchesRawValue() {
+        XCTAssertEqual(Storefront.canada.id, "ca")
+        XCTAssertEqual(Storefront.spain.id, "es")
+    }
 }
