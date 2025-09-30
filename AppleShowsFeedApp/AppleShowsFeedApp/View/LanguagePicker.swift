1d046de7d6d8c1e1e231adb24fa33db481e29415
omaestra <omaestra@gmail.com>
2025-09-30 13:35:12 +0200

    Update picker localized title key.

--- AppleShowsFeedApp/AppleShowsFeedApp/View/LanguagePicker.swift
+++ AppleShowsFeedApp/AppleShowsFeedApp/View/LanguagePicker.swift
@@ -11,7 +11,7 @@ struct LanguagePicker: View {
     @Binding var selectedCountry: Storefront
 
     var body: some View {
-        Picker("Country", selection: $selectedCountry) {
+        Picker("Storefront", selection: $selectedCountry) {
             ForEach(Storefront.allCases) { country in
                 Text(country.displayName).tag(country)
             }
